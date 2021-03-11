82b380f772798543b7a148bdf9453e645b8f3f9d
Ed Chekanov <kide.da@gmail.com>
2021-03-11 16:34:33 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 > 0) {
+    return false;
+  };
+
+  let item = str;
+  let i = bracketsConfig.length - 1;
+
+  function hex() {
+  let index1 = bracketsConfig[i][0];
+  let index2 = bracketsConfig[i][1];
+  item = item.split(index1 + index2).join('');
+  i--;
+  }
+
+  function circle() {
+  itemLength1 = item.length;
+  while (i >= 0) {
+    hex();
+  }
+  itemLength2 = item.length;
+  if (itemLength1 > itemLength2) {
+    i = bracketsConfig.length - 1;
+    circle();
+  } 
+  }
+
+  circle();
+  if (item.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
